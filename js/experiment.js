45f00e6189fec0e60360efe1ebe39782bd3240a0
Arthi Vezhavendan <arthivez@mit.edu>
2014-08-06 01:34:19 -0400

    fully recording reaction times
    todo - remove progress bar

--- js/experiment.js
+++ js/experiment.js
@@ -1,3 +1,19 @@
+/*TODO:
+	- progress bar, if necessary
+	- input from excel spreadsheet instead of writing out arrays manually
+	- disable phone use
+			potentially with this in html header:
+				<script type="text/javascript">
+				  <!--
+				  if (screen.width <= 800) {
+				    window.location = "http://m.domain.com";
+				  }
+				  //-->
+				</script>
+	- record times in experiment variable
+
+*/
+
 
 //first some things useful for randomizing conditions:
 
@@ -43,6 +59,8 @@ function showNextSlide() {
     	isRecording = true;
     }
     if(slides[slideStage] == 'finished'){   //if 'finished' slide, submit experiment to turk
+    	experiment.timer("endtrial");
+    	isRecording = false;
     	setTimeout(function() { turk.submit(experiment) }, 1500);
     }
 
@@ -80,16 +98,22 @@ function randomOrder(statements) {
 	return statements;
 }
 
+
+/*
+experiment = {
+	trials:  [ {story: story0, intro: introduction sentence, words: full sentence, wordsTimes: [(word, time), (word, time)]}, 
+	]
+
+}
+*/
+
 var experiment = {
 	times: {},
     timer: function(stamp) {
 		this.times[stamp] = (new Date()).getTime();
 	},
-    stories: stories,
+    //stories: stories, //TODO: fix with proper sentences when you gather them.
     totalTrials: stories.length,
-    
-    //totalEyesTrials: storiesEyes.length,    
-    trial: 0, //first trial will be trial number 0
     trials: [],
     demographics: {},
 	//**current_story: "",
@@ -99,172 +123,40 @@ var experiment = {
 		//var story = this.stories[this.trial];
 		//**this.current_story = story.shortname; //for checking when we've changed.
 		//$('#s1').html(story.s1);
-		//this.timer("starttrial");
+		this.timer("starttrial");
 		//this.nextSentence();
 		//showSlide("questions");
 	},
 
-	checkForNextWord: function(spacebarTime) {
-		if(!wordTesting) {
-			return;
-		}
-		console.log("word testing!");
-		//nextWord(spacebarTime);
+
+	record: function(fullSent, wordTimes, prevStoryNumber) {
+		//if 
+		console.log("RECORDED!");
+
+		this.trials.push({ "story": prevStoryNumber,
+							//"intro": storiesX[prevStoryNumber][0],
+							"fullStory": fullSent,
+							"wordTimes": wordTimes});
 	},
-/*
-	nextWord: function(sentence) {
-		wordTesting = true;
-		console.log(sentence);
-		var words = sentence.split(" ");
-		console.log(words);
-		current = 0;
-		rxnTimes = [];
-		while(current < words.length) {
-			//console.log("while current<length");
-			$('#word').html(words[current]);
-			//console.log(keypressed);
-			var t1 = new Date().getTime();
-			scraps = [];
-			scrap = 0;
-			while(!keypressed){
-			//	rxnTime = (new Date()).getTime();
-				//keep looping until a key is pressed;
-				scrap = new Date().getTime();
-				scraps.push(scrap);
-				//if(isInfinite(t1,3000)){
-        		//	alert('Loop stopped after 3 seconds')
-        		//	break;
-    			//} 
-			}
-
-			
-			//console.log(keypressed);
-			//console.log("___________");
-			//rxnTime = new Date().getTime();
-			keypressed = false;
-			here = current + sentence;
-			rxnTimes.push(here);
-			current++;
-		}
-		$('#word').html("done...ready?");
-		wordTesting = false;
-		console.log(rxnTimes);
-		return rxnTimes;
-	},
-*/
-	//need a record of the current word displayed
-
-/*	nextWordY: function(pressTime, prevWord, prevStoryNumber, prevWordNumber) { //spacebar was clicked - so display the next word. next -- record stop time
-		console.log(pressTime); //record time here, with previous word
-		console.log((new Date()).getTime);
-		console.log("******");
-
-	//see if prevWord was the final word in the sentence.
-		//if prevWord is not final word:
-		if(prevWordNumber < this.storiesX[prevStoryNumber].length){
-			//display the nextWord ---and wait for key press.
-			this.wordNumber++;
-			$('#word').html(this.storiesX[prevStoryNumber][wordNumber]);
-			console.log("next word displayed RIGHT?");
-		}
-		//else, when lastWord is the final word in the sentence:
-		else {
-			//update currentStory with getNextSentence
-			this.storyNumber++;
-			this.wordNumber = 0;
-			console.log("story number and word number RESET-ED");
-		}
-
-			
-			//and display the "Ready?" ---so when they press space, the intro sentence is displayed (a word in next story)
-				//(while "ready?" is displayed, when space hit, nextWordX will be called and the word in currentstory will display)
-
-
-
-
-
-
-	},*/
-/*
-	nextSentence: function() {
-		$('#word').html("Ready?????");
-		var sentence = this.stories[this.trial].sentpos; //TODO: depending on the trial number, we should choose sentPos, sentNeg, etc.
-		this.timer("starttrial"); //TODO: show intro story sentence, and then word by word.
-		//rxnTimes = nextWord(sentence); //show next word and record time between each word
-		rxnTimes = this.nextWord(sentence); //show next word and record time between each word
-		experiment.record(rxnTimes, this.trial);
-
-		this.trial++;
-		$('.bar').css('width', (200.0 * this.trial/this.totalTrials) + 'px');	//advance the completion bar at top
-		
-		if (this.trial >= this.totalTrials) {
-			showNextSlide(); //completed all the sentences, so show final slides
-			return;}
-		
-		//if there is another sentence to check:
-		if(this.trial < this.stories.length){
-			this.nextSentence();
-		} else {
-			showNextSlide();
-
-		}
-	},
-	*/
-	
-	record: function(trial, emp) {
-		// results={"a1": emp};
-		// this.trialsEQ.push({	"trial": trial,
-		// 					"story": this.storiesEQ[this.trial].story, 	//empath# or warmup#
-		// 					"ptype": this.storiesEQ[this.trial].ptype, 	//actual or warmup
-		// 					"s1": this.storiesEQ[this.trial].s1,			//"statement"
-		// 					"rt": this.times.stoptrial - this.times.starttrial,
-		// 					"results": results});						//"a1": stronglyagree/slightlyagree/slightlydisagree/stronglydisagree
-	},
-	
-    //this fuction get's called to add a time stamp: each time we move on to the next phase.
-    times: {},
-    timer: function(stamp) {
-		this.times[stamp] = (new Date()).getTime();
-    },
 
 };
 
 var storyNumber = 0;
 var wordNumber = -1;  //0 = "Ready?" prompt, 1 = Introduction Sentence, 2+ = Words in sentence.
+var currentWordTimes = [];
+var fullSentence = ""; //TODO: modify with master list of sentences
 
+//storiesX is an array of arrays of the word parts to be displayed. 
+	///It is made at the start during consent, to reduce any lag.
 storiesX = [["Ready?", "This is Tom and Sally.", "Tom", "gave", "Sally", "a", "flower."], 
 			["Ready?", "Here comes Roger and Nimmy.", "Nimmy", "walked", "faster", "than", "Roger."],
 			["Ready?", "This is a question...", "Answer - ", "Yes or No?"],
 			];
 
 
-
-$(document).keypress(function(e) {
-  if(e.which == 32) {
-    // spacebar pressed
-    var pressTime = (new Date()).getTime();
-    var wordOnScreen = $('#word').html();
-    
-    console.log("----------------------------------------");
-    if(isRecording){
-	    nextWordX(pressTime, wordOnScreen, storyNumber, wordNumber);
-	}
-  }
-});
-
-
-function isInfinite(t1,timeLimit){
-    	var t2 = new Date().getTime();
-   	 	if(t2-t1> timeLimit){
-       		return true;
-   		}
-    	else return false;
-	};
-
 function nextWordX(pressTime, prevWord, prevStoryNumber, prevWordNumber) { //spacebar was clicked - so display the next word. next -- record stop time
-		//console.log(pressTime); //record time here, with previous word
-		//console.log((new Date()).getTime);
-		//console.log("******");
+		currentWordTimes.push([prevWord, pressTime]);
+		fullSentence = fullSentence + " " + prevWord;
 
 	//see if prevWord was the final word in the sentence.
 		//if prevWord is not final word:
@@ -285,17 +177,34 @@ function nextWordX(pressTime, prevWord, prevStoryNumber, prevWordNumber) { //spa
 			wordNumber = -1;
 			console.log("story number and word number RESET-ED");
 
-			//if done with stories, go to end slide:
+			//if done with all stories, go to end slide:
 			if(storyNumber >= storiesX.length){
 				showNextSlide(); //go to Finished Slide
 			}
-			//else prompt the next sentence:
-			nextWordX((new Date()).getTime(), 'start...', storyNumber, wordNumber) 
+			//else record story data and prompt the next sentence:
+			experiment.record(fullSentence, currentWordTimes, prevStoryNumber);
+			currentWordTimes = [];
+			fullSentence = "";
+			nextWordX((new Date()).getTime(), '', storyNumber, wordNumber) //TODO: this line is necessary, but it's funky in turk info
 
 		}//and display the "Ready?" ---so when they press space, the intro sentence is displayed (a word in next story)
 				//(while "ready?" is displayed, when space hit, nextWordX will be called and the word in currentstory will display)
 
 	};
+
+
+$(document).keypress(function(e) {
+  if(e.which == 32) {
+    // spacebar pressed
+    var pressTime = (new Date()).getTime();
+    var wordOnScreen = $('#word').html();
+    
+    console.log("----------------------------------------");
+    if(isRecording){
+	    nextWordX(pressTime, wordOnScreen, storyNumber, wordNumber);
+	}
+  }
+});
 
 
 
